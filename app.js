fe51526b9310b80e18643941b28439d125d1e75c
Kevin Bridges <KBridges@alegeus.com>
2018-05-30 08:42:40 -0400

    able to import data.json as an object

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 console.log('---');
 
-import test from './data/data.json';
+//import test from './data/data.json';
 
-var json_data = json.stringify(test);
+//var json_data = json.stringify(test);
 
 
-console.log(json_data);
+//console.log(json_data);
+
+
+
+$.getJSON('./data/data.json', function(data) {
+    console.log(data);
+});
